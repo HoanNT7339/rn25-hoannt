b3d4ba9185d5f45d1c47650d79732a79e641a7af
hoannt <tienhoan.qcd1@gmail.com>
2022-10-05 21:07:01 +0700

    bailamcoban

--- rn25-hoannt/src/App.tsx
+++ rn25-hoannt/src/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     Hello World
-    </div>
+    <>
+    <head></head>
+    <body>
+      <div className="div1">The Pulpit Rock</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <ul>
+              <li><a href={"#"} target="_blank">The Drive</a></li>
+              <li><a href={"#"} target="_blank">The Drive</a></li>
+              <li><a href={"#"} target="_blank">The Drive</a></li>
+              <li><a href={"#"} target="_blank">The Drive</a></li>
+            </ul>
+          </div>
+          <div className="col-sm">
+            <h1>The Walk</h1>
+            <span>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</span>
+          </div>
+          <div className="col-sm">One of three columns</div>
+        </div>
+      </div>
+    </body>
+    <footer>
+      Copy right @@@
+    </footer>
+    </>
   );
 }
 
