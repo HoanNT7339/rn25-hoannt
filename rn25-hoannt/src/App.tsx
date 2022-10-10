bafe6a677327ce860ad57db731caf5ada6cde24d
hoannt <tienhoan.qcd1@gmail.com>
2022-10-10 18:29:55 +0700

    baitap

--- rn25-hoannt/src/App.tsx
+++ rn25-hoannt/src/App.tsx
@@ -1,34 +1,45 @@
 import React from "react";
-import logo from "./logo.svg";
+import img from "./assets/images/1.jpg";
 import "./App.css";
 
 function App() {
   return (
     <>
-    <head></head>
-    <body>
-      <div className="div1">The Pulpit Rock</div>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm">
-            <ul>
-              <li><a href={"#"} target="_blank">The Drive</a></li>
-              <li><a href={"#"} target="_blank">The Drive</a></li>
-              <li><a href={"#"} target="_blank">The Drive</a></li>
-              <li><a href={"#"} target="_blank">The Drive</a></li>
-            </ul>
+      <div className="container-fluid">
+        <div className="title_header">
+          <h1>The Pulpit Rock</h1>
+        </div>
+        <div className="row m-3">
+          <div className="col-md-3">
+            <div className="border-bottom text-info bg-gray"><p>text 1</p></div>
+            <div className="border-bottom text-info bg-gray"><p>text 1</p></div>
+            <div className="border-bottom text-info bg-gray"><p>text 1</p></div>
+            <div className="border-bottom text-info bg-gray"><p>text 1</p></div>
           </div>
-          <div className="col-sm">
+          <div className="col-md-6 text-center pt-3">
             <h1>The Walk</h1>
-            <span>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</span>
+            <p>text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here text here</p>
+            <img className="mainImage" src={img} alt={""}/> 
           </div>
-          <div className="col-sm">One of three columns</div>
+          <div className="col-md-3 bg-info h-50">
+          <div className="border-bottom">
+            <p>text 1</p>
+            <p style={{fontSize: "12px",color: "green"}}>text here text here text here text here text here text here text here text here text here text here text here text here </p>
+          </div>
+          <div className="border-bottom">
+            <p>text 1</p>
+            <p style={{fontSize: "12px",color: "green"}}>text here text here text here text here text here text here text here text here text here text here text here text here </p>
+          </div>
+          <div className="border-bottom">
+            <p>text 1</p>
+            <p style={{fontSize: "12px",color: "green"}}>text here text here text here text here text here text here text here text here text here text here text here text here </p>
+          </div>
+          </div>
+        </div>
+        <div className="footer d-block">
+          <p className="text-center pt-4">text here text here text here text here text here text here text here text here text here text here text here text here text here </p>
         </div>
       </div>
-    </body>
-    <footer>
-      Copy right @@@
-    </footer>
     </>
   );
 }
